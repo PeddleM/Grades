b60e19cd4c2acaecf6bf1d0019961f5d245e8bcc
Michael Peddle <michael.peddle@ntlworld.com>
2015-05-11 20:39:57 +0100

    Methods, params plus overloading lecture

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -8,35 +8,55 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            GradeBook g1 = new GradeBook();
-            GradeBook g2 = g1;
+            GradeBook book = new GradeBook();
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+            book.AddGrade(75f);
 
-            g1 = new GradeBook();
+            GradeStatistics stats = book.ComputeStatistics();
 
-            g1.Name = "Michaels book";
-            Console.WriteLine(g2.Name);
+            WriteNames("Scott", "Michael", "Steven");
 
-            int x1 = 4;
-            int x2 = x1;
+            int number = 20;
+            WriteBytes(number);
+            WriteBytes(stats.AverageGrade);
 
-            x1 = 100;
-
-            Console.WriteLine(x2);
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
 
             Console.ReadLine();
 
-
-            //GradeBook book = new GradeBook();
-            //book.AddGrade(91);
-            //book.AddGrade(89.5f);
-            //book.AddGrade(75f);
-
-            //GradeStatistics stats = book.ComputeStatistics();
-            //Console.WriteLine(stats.AverageGrade);
-            //Console.WriteLine(stats.LowestGrade);
-            //Console.WriteLine(stats.HighestGrade);
-
-            //Console.ReadLine();
         }
+
+        private static void WriteBytes(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteBytes(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteByteArray(byte[] bytes)
+        {
+            foreach (byte b in bytes)
+            {
+                Console.Write("0x{0:X} ", b);
+            }
+            Console.WriteLine();
+        }
+        
+        private static void WriteNames(params string[] names)
+        {
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
     }
 }
